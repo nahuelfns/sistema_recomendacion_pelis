ae81e83843f23894d768f3515bef3da4ed118c42
Facundo Nahuel <114108574+nahuelfns@users.noreply.github.com>
2024-12-28 10:24:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 
 app = FastAPI()
 
-df = pd.read_csv(r'Datasets/movies_dataset_transf.csv',low_memory=False) 
+df = pd.read_csv(r'Datasets/df_para_funciones.csv',low_memory=False) 
 df_get_actor = pd.read_csv(r'Datasets/df_para_funcion_get_actor.csv',low_memory=False)
 df_get_director = pd.read_csv(r'Datasets/df_para_funcion_get_director.csv',low_memory=False)
 df_para_recomendacion = pd.read_csv(r'Datasets/df_para_recomendacion.csv', low_memory=False)
